226d215f5ff6239e763a5859c463588e8c894e18
Your Name <ahmed.do7a831@gmail.com>
2024-10-03 17:51:28 +0300

    create notes view body and custom app bar and custom search icon

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Notes ',
+          style: TextStyle(fontSize: 30),
+        ),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
