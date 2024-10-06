dceeca2e4a71c4c40def8cacbfc2ac0a150f4f5a
Your Name <ahmed.do7a831@gmail.com>
2024-10-06 05:53:30 +0300

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,58 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadiusDirectional.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Text(
-                'Build Your Carrer With Ahmed Mamdouh',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadiusDirectional.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                    color: Colors.black.withOpacity(0.6), fontSize: 18),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.delete,
                   color: Colors.black,
-                  size: 34,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  'Build Your Carrer With Ahmed Mamdouh',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.6), fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                    size: 34,
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'may21,2022',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.6),
-                fontSize: 16,
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'may21,2022',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.6),
+                  fontSize: 16,
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
