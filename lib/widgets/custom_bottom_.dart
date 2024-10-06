13bf1f5819eff33d0cfe78bd29e1628e97c64994
Your Name <ahmed.do7a831@gmail.com>
2024-10-06 05:44:28 +0300

    finish notes bottom sheet

---
+++ lib/widgets/custom_bottom_.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widgets/constant.dart';
+
+class CustomBottom extends StatelessWidget {
+  const CustomBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: KprimaryColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      child: const Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/note_bottom_sheet.dart
+++ lib/widgets/note_bottom_sheet.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:note_app/widgets/custom_bottom_.dart';
 import 'package:note_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -9,22 +9,31 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hint: 'Title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: 'Content',
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 150,
+            ),
+            CustomBottom(),
+            SizedBox(
+              height: 32,
+            ),
+          ],
+        ),
       ),
     );
   }
