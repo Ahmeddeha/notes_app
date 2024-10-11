7c2f64cac035b1d577c43f6b5735e4ff58249df4
Your Name <ahmed.do7a831@gmail.com>
2024-10-11 03:43:24 +0300

    display notes part 1

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/widgets/constant.dart';
 
@@ -9,16 +10,9 @@ part 'notes_state.dart';
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
+  List<NoteModel>? notes;
   fetchAllNotes() {
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      emit(
-        NotesSuccess(
-          notes: notesBox.values.toList(),
-        ),
-      );
-    } catch (e) {
-      emit(NotesFaliuer(errMessage: e.toString()));
-    }
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes = notesBox.values.toList();
   }
 }
--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -3,18 +3,4 @@ part of 'notes_cubit.dart';
 @immutable
 sealed class NotesState {}
 
-final class NotesInitial extends NotesState {}
-
-class NotesLoading extends NotesState {}
-
-class NotesSuccess extends NotesState {
-  final List<NoteModel> notes;
-
-  NotesSuccess({required this.notes});
-}
-
-class NotesFaliuer extends NotesState {
-  final String errMessage;
-
-  NotesFaliuer({required this.errMessage});
-}
+class NotesInitial extends NotesState {}
